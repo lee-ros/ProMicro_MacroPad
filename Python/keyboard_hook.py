4465d72f3d7a9f7e66f863b03d9ffdb81298f61c
Lee Rosenberg <laluk321@gmail.com>
2021-08-11 01:57:04 +0300

    added bindings for F13-F21 keys

--- Python/keyboard_hook.py
+++ Python/keyboard_hook.py
@@ -1,9 +1,20 @@
 import keyboard
 from time import sleep
 
+from actoins import run_app
+#import actions
+
 # Define key bindings
-keyboard.add_hotkey('F13', lambda: print('F13 Pressed'))
+keyboard.add_hotkey('F13', run_app, args=[
+                    r"C:\Program Files\PuTTY\putty.exe"])
 keyboard.add_hotkey('F14', lambda: print('F14 Pressed'))
+keyboard.add_hotkey('F15', lambda: print('F14 Pressed'))
+keyboard.add_hotkey('F16', lambda: print('F14 Pressed'))
+keyboard.add_hotkey('F17', lambda: print('F14 Pressed'))
+keyboard.add_hotkey('F18', lambda: print('F14 Pressed'))
+keyboard.add_hotkey('F19', lambda: print('F14 Pressed'))
+keyboard.add_hotkey('F20', lambda: print('F14 Pressed'))
+keyboard.add_hotkey('F21', lambda: print('F14 Pressed'))
 
 # Sleeping to keep script alive
 while True:
