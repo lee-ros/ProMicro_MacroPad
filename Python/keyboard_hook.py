ad08e6bbf65666ad86e313fb0768036549e813ac
Lee Rosenberg <laluk321@gmail.com>
2021-08-13 00:11:08 +0300

    added dict for key:function mapping and implemented hotkey assignment through a for loop

--- Python/keyboard_hook.py
+++ Python/keyboard_hook.py
@@ -4,17 +4,24 @@ from time import sleep
 from actoins import run_app, open_folder
 #import actions
 
+# key:function mapping
+key_functions = {
+    'F13': lambda: print('F13 is pressed'),
+    'F14': lambda: print('F14 is pressed'),
+    'F15': lambda: print('F15 is pressed'),
+    'F16': lambda: print('F16 is pressed'),
+    'F17': lambda: print('F17 is pressed'),
+    'F18': lambda: print('F18 is pressed'),
+    'F19': lambda: print('F19 is pressed'),
+    'F20': lambda: print('F20 is pressed'),
+    'F21': lambda: print('F21 is pressed'),
+}
+
+
 # Define key bindings
-keyboard.add_hotkey('F13', run_app, args=[
-                    r"C:\Program Files\PuTTY\putty.exe"])
-keyboard.add_hotkey('F14', open_folder, args=[r"C:"])
-keyboard.add_hotkey('F15', lambda: print('F14 Pressed'))
-keyboard.add_hotkey('F16', lambda: print('F14 Pressed'))
-keyboard.add_hotkey('F17', lambda: print('F14 Pressed'))
-keyboard.add_hotkey('F18', lambda: print('F14 Pressed'))
-keyboard.add_hotkey('F19', lambda: print('F14 Pressed'))
-keyboard.add_hotkey('F20', lambda: print('F14 Pressed'))
-keyboard.add_hotkey('F21', lambda: print('F14 Pressed'))
+for key, value in key_functions.items():
+    keyboard.add_hotkey(key, value)
+
 
 # Sleeping to keep script alive
 while True:
