15051a4889836ad49298545cc936eb65df3b83a8
Lee Rosenberg <laluk321@gmail.com>
2021-08-12 23:49:55 +0300

    added open_folder ation

--- Python/actoins.py
+++ Python/actoins.py
@@ -8,10 +8,21 @@ def run_app(path):
     Params:
         path - an absolute path to .exe file
     '''
+    if(not os.path.exists(path)):
+        raise FileNotFoundError('The path does not exists')
     if(os.access(path, os.X_OK)):
         subprocess.Popen(path)
     else:
-        if(os.path.exists(path)):
-            raise Exception('The path is not an executable file')
-        else:
-            raise FileNotFoundError('The path is not an executable file')
+        raise Exception('The path is not an executable file')
+
+
+def open_folder(path):
+    '''
+    this function is used to launch the explorer at the given folder
+    Params:
+        path - an absolute path to a folder
+    '''
+    if(os.path.isdir(path)):
+        subprocess.Popen(f'explorer.exe {path}')
+    else:
+        raise Exception('The path given is not a directory')
