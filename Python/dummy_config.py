68634798d20882be8f4fa69f22a05cdf97b16dc9
Lee Rosenberg <laluk321@gmail.com>
2021-08-14 21:39:20 +0300

    added dummy_config setup file

---
+++ Python/dummy_config.py
@@ -0,0 +1,38 @@
+dummy_config = {
+    'F13': {
+        'action': '',
+        'args': ''
+    },
+    'F14': {
+        'action': '',
+        'args': ''
+    },
+    'F15': {
+        'action': '',
+        'args': ''
+    },
+    'F16': {
+        'action': '',
+        'args': ''
+    },
+    'F17': {
+        'action': '',
+        'args': ''
+    },
+    'F18': {
+        'action': '',
+        'args': ''
+    },
+    'F19': {
+        'action': '',
+        'args': ''
+    },
+    'F20': {
+        'action': '',
+        'args': ''
+    },
+    'F21': {
+        'action': '',
+        'args': ''
+    },
+}
